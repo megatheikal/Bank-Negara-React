cc459f399e5babfbe85e812c7ce2447ebacaa466
m00m <mgaat.hkl@gmail.com>
2021-01-19 16:41:13 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import web3 from "./web3";
+import bankNegara from "./bankNegara";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bank: "",
+      balance: "",
+      value: "",
+      limit: "",
+      withdrawvalue: ""
+    };
+  }
+
+  async componentDidMount() {
+    const bank = await bankNegara.methods.bank().call();
+    const balance = await bankNegara.methods.getBalance().call();
+    const limit = await bankNegara.methods.getLimit().call();
+    //const suspect = await bankNegara.methods.infos().call();
+    const infoCount = await bankNegara.methods.getInfoCount().call();
+
+    this.setState({ bank, balance, limit });
+  }
+
+  onDeposit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await bankNegara.methods.deposit(web3.utils.toBN(this.state.value)).send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether")
+    });
+  };
+
+  onWithdraw = async event => {
+    try {
+      event.preventDefault();
+
+      const accounts = await web3.eth.getAccounts();
+
+      await bankNegara.methods
+        .withdraw(this.state.withdrawvalue, accounts[0])
+        .sendTransaction({
+          from: "0xb9DC008BEa28A56341460E5520D5C4bC2081e0a9",
+          value: web3.utils.toWei(this.state.withdrawvalue, "ether")
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Bank Negara Smart Contract</h1>
+
+        <h2>
+          The current balance in the bank is{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether and the limit
+          of any transaction happen in the bank is{" "}
+          {web3.utils.fromWei(this.state.limit, "ether")}. The address of this
+          smart contract is 0x23EFfF5d0053238d70454296f699350b11532408
+        </h2>
+        <p>This smart contract been operated by {this.state.bank} </p>
+        <hr />
+        <form onSubmit={this.onDeposit}>
+          <div>
+            <label>Amount of ether to deposit</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Deposit !</button>
+        </form>
+        <hr />
+        <form onSubmit={this.onWithdraw}>
+          <div>
+            <label>Amount of ether to withdraw</label>
+            <input
+              value={this.state.withdrawvalue}
+              onChange={event =>
+                this.setState({ withdrawvalue: event.target.value })
+              }
+            />
+          </div>
+          <button>Withdraw !</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/bankNegara.js
@@ -0,0 +1,103 @@
+import web3 from "./web3";
+import { ethers } from "ethers";
+
+const address = "0x23EFfF5d0053238d70454296f699350b11532408";
+
+const abi = [
+  {
+    constant: false,
+    inputs: [
+      { name: "_amount", type: "uint256" },
+      { name: "_receiver", type: "address" }
+    ],
+    name: "withdraw",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getBalance",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [{ name: "_limit", type: "uint256" }],
+    name: "setLimit",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "infos",
+    outputs: [
+      { name: "owner", type: "address" },
+      { name: "amount", type: "uint256" }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "bank",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "owner",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getLimit",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [{ name: "_amount", type: "uint256" }],
+    name: "deposit",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getInfoCount",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+
+web3.eth.defaultAccount = "0x7EAF08Cb795C2341cf49C292BcdAd66931CFC8E1";
+export default new web3.eth.Contract(abi, address);
